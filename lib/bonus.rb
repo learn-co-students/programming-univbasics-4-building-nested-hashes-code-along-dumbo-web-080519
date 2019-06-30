a501dfafdd0b5c29717e943859458c94e4a7080a
Josh Nho <joshnho@gmail.com>
2019-06-30 01:56:08 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,6 +24,7 @@ def bonus
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 
+# I tested this out in VScode and it seems to update the status of hero and heroine just fine. I'm not sure why I'm not able to pass this lab.
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
